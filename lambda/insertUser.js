f1944f3ae00a3bb674d6791efcd0ff3ee17edb4a
ryans <yoon91dev@gmail.com>
2017-11-23 00:16:22 +0900

    [0.0.1] 코드 개선

--- lambda/insertUser.js
+++ lambda/insertUser.js
@@ -2,66 +2,53 @@
 
 var mysql = require('mysql');
 var config = require('./config.js');
+var error = require('./error/error.js');
 
 exports.insertUser = function (event, context, callback) {
 
     let userId = event['user_id'];
 
-    if (config.env_dev) {
-        userId = 'debug_user';
+    if (!userId) {
+        config.response(error.user_requireID, callback);
+        return;
     }
 
     let connection = mysql.createConnection({
-        host     : config.host,
-        user     : config.user,
-        password : config.password,
-        database : config.database
+        host: config.host,
+        user: config.user,
+        password: config.password,
+        database: config.database
     });
 
-    let alreadyErr = {
-        "result" : 100
-    };
-
     connection.connect();
-    connection.query('SELECT * FROM sentenceDB.users WHERE `user_id` = "' + userId + '";',
+    connection.query('SELECT * FROM '+ config.db_table_users +' WHERE `user_id` = "' + userId + '";',
         function (err, rows, fields) {
             if (!err) {
                 if (rows.length > 0) {
-                    //아이디 존재
-                    if (!config.env_dev) {
-                        callback(null, alreadyErr);
-                    } else {
-                        console.log('Already exist user');
-                    }
+                    config.response(error.user_alreadyExist, callback);
                     connection.end();
                 } else {
                     //신규유저
-                    inputUser(connection, userId, callback);
+                    connection.query('INSERT INTO '+ config.db_table_users +' (`user_id`)' +
+                        'VALUES ("' + userId + '")',
+                        function (err, rows, fields) {
+                            if (!err) {
+                                config.response({
+                                    msg : '회원가입 성공'
+                                }, callback);
+                                connection.end();
+                            } else {
+                                config.response(error.db_error, callback);
+                                connection.end();
+                            }
+
+                        }
+                    );
                 }
             } else {
-                callback(null, error);
-                console.log('DB에러');
+                config.response(error.db_error, callback);
                 connection.end();
             }
         }
     );
 };
-
-function inputUser(connection, id, callback) {
-    connection.query('INSERT INTO `sentenceDB`.`users` (`user_id`)' +
-    	                    'VALUES ("' + id + '")',
-    	function (err, rows, fields) {
-            if (!err) {
-                if (!config.env_dev) {
-                    callback(null, sucessUser);
-                } else {
-                    console.log('회원가입 완료');
-                }
-            } else {
-                callback(null, error);
-                console.log('DB에러');
-            }
-
-    	});
-    connection.end();
-}
